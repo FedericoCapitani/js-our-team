05b80f978433bb6f0e610dc3d99cd231a74d4bc5
FedericoCapitani <federico.capitani@outlook.it>
2022-04-07 16:53:56 +0200

    aggiunte foto al profile dei membri

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,16 +5,17 @@ stampare su console le informazioni di nome, ruolo e la stringa della foto
 MILESTONE 2 
 stampare le stesse informazioni su DOM sottoforma di stringhe */
 
-function stampa_info_oggetto(object){
+function stampa_info_oggetto(object, array_img){
     const info = document.getElementById('team')
     info.innerHTML = ''
     for(let i = 0; i < object.length; i++){
         const info_name = document.createElement('div');
         const info_role = document.createElement('div');
         const info_photo = document.createElement('div');
+        info_photo.classList.add('photo');
         info_name.append(`Nome: ${object[i].name}`)
         info_role.append(`Ruolo: ${object[i].role}`)
-        info_photo.append(`Foto: ${object[i].image}`)
+        info_photo.append(array_img[i])
         info.append(info_name, info_role, info_photo)
     }
 }
@@ -51,11 +52,27 @@ const team = [
       role: 'Graphic Designer',
       image: 'barbara-ramos-graphic-designer.jpg',
     },
-  ];
+];
+
+var photo_membri = []
+photo_membri[0] = new Image()
+photo_membri[0].src = '../assets/img/new-team-member-04.jpg'
+photo_membri[1] = new Image()
+photo_membri[1].src = '../assets/img/new-team-member-03.jpg'
+photo_membri[2] = new Image()
+photo_membri[2].src = '../assets/img/walter-gordon-office-manager.jpg'
+photo_membri[3] = new Image()
+photo_membri[3].src = '../assets/img/angela-lopez-social-media-manager.jpg'
+photo_membri[4] = new Image()
+photo_membri[4].src = '../assets/img/wayne-barnett-founder-ceo.jpg'
+photo_membri[5] = new Image()
+photo_membri[5].src = '../assets/img/barbara-ramos-graphic-designer.jpg'
+
+console.log(photo_membri);
 console.log('Membri del team:');
 for(let i = 0; i < team.length; i++){
     console.log(`Nome: ${team[i].name}`);
     console.log(`Ruolo: ${team[i].role}`);
     console.log(`Foto: ${team[i].image}`);
 }
-const membri_team = stampa_info_oggetto(team);
+const membri_team = stampa_info_oggetto(team, photo_membri);
